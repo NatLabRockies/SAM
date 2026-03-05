6ae196882e4343b0015012df9e5e06c97d6b7cdb
Steven Janzou <steven@janzouconsulting.com>
2026-03-05 03:08:47 -0700

    Update code generator to include 9 required libraries for ssc to run
     "abseil_dll", "bz2", "highs", "libprotobuf", "libscip", "libutf8_validity", "ortools", "re2", "ssc", "zlib1" in CodeGen_Base::PlatformFiles
    Tested on Windows with VBA MSPT SO and MP and PVWatts

--- src/codegenerator.cpp
+++ src/codegenerator.cpp
@@ -175,27 +175,39 @@ CodeGen_Base::~CodeGen_Base()
 bool CodeGen_Base::PlatformFiles()
 {
 	// copy appropriate dll
+	// updated for ortools Windows - minimum set of ortools dlls to run SSC modules that use ortools optimization:
+	// abseil_dll.dll, bz2.dll, highs.dll, libprotobuf.dll, libscip.dll, libutf8_validity.dll, ortools.dll, re2.dll, ssc.dll, zlib1.dll
+	wxArrayString as = { "abseil_dll", "bz2", "highs", "libprotobuf", "libscip", "libutf8_validity", "ortools", "re2", "ssc", "zlib1"};
 #if defined(__WXMSW__) && defined (__64BIT__)
-	wxString f1 = SamApp::GetAppPath() + "/ssc.dll";
-	wxString f2 = m_folder + "/ssc.dll";
-#elif defined(__WXMSW__) && defined(__32BIT__)
-	wxString f1 = SamApp::GetAppPath() + "/sscx32.dll";
-	wxString f2 = m_folder + "/ssc.dll";
+	wxString ext = ".dll";
+	wxString source_folder = SamApp::GetAppPath();
+//	wxString f1 = SamApp::GetAppPath() + "/ssc.dll";
+//	wxString f2 = m_folder + "/ssc.dll";
 #elif defined(__WXOSX__)
-	wxString f1 = SamApp::GetAppPath() + "/../Frameworks/ssc.dylib";
-	wxString f2 = m_folder + "/ssc.dylib";
+	wxString ext = ".dylib";
+	wxString source_folder = SamApp::GetAppPath() + "/../Frameworks";
+//	wxString f1 = SamApp::GetAppPath() + "/../Frameworks/ssc.dylib";
+//	wxString f2 = m_folder + "/ssc.dylib";
 #elif defined(__WXGTK__)
-	wxString f1 = SamApp::GetAppPath() + "/ssc.so";
-	wxString f2 = m_folder + "/ssc.so";
+	wxString ext = ".so";
+	wxString source_folder = SamApp::GetAppPath();
+//	wxString f1 = SamApp::GetAppPath() + "/ssc.so";
+//	wxString f2 = m_folder + "/ssc.so";
 #else
 	return false;
 #endif
-	wxCopyFile(f1, f2);
+	for (size_t i = 0; i < as.size(); i++) {
+		wxString f1 = source_folder + "/" + as[i] + ext;
+		wxString f2 = m_folder + "/" + as[i] + ext;
+		if (wxFileExists(f1))
+			wxCopyFile(f1, f2);
+	}
+
 	// sscapi.h - switch to copy version for syching issues 5/30/17
 	// assumes in runtime folder for all builds. See SAM issue 1918
-	f1 = SamApp::GetRuntimePath() + "/sscapi.h";
+	wxString f1 = SamApp::GetRuntimePath() + "/sscapi.h";
 	if (wxFileExists(f1)) {
-		f2 = m_folder + "/sscapi.h";
+		wxString f2 = m_folder + "/sscapi.h";
 		wxCopyFile(f1,f2);
 	}
 	return true;
